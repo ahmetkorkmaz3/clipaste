966c6b13ee9abbb01594df6572f16cdcf7809344
ahmetkorkmaz3 <ahmetkorkmaz3453@gmail.com>
2020-04-01 00:16:33 +0300

    Olmadı

--- main.js
+++ main.js
@@ -1,27 +1,65 @@
-const {app, BrowserWindow, screen} = require('electron')
-const path = require('path')
+const {
+  app,
+  BrowserWindow,
+  screen,
+  globalShortcut,
+  clipboard
+} = require("electron");
+const path = require("path");
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
 
-function createWindow () {
+const adapter = new FileSync(path.join(app.getPath("home"), "/.clipaste.json"));
+const db = low(adapter);
+
+db.defaults({ clipboard: [] }).write();
+
+function createWindow() {
+  let appShow = false;
+
+  const copyShortcut = process.platform === "darwin" ? "Option+C" : "Alt+C";
+  const pasteShortcut = process.platform === "darwin" ? "Option+V" : "Alt+V";
+
   const mainWindow = new BrowserWindow({
     frame: false,
     width: 400,
     height: screen.getPrimaryDisplay().workAreaSize.height,
-    x: (screen.getPrimaryDisplay().workAreaSize.width - 400),
+    x: screen.getPrimaryDisplay().workAreaSize.width - 400,
     y: 0,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+      preload: path.join(__dirname, "preload.js")
+    },
+    show: false
+  });
 
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile("index.html");
+
+  globalShortcut.register("CmdOrCtrl+Shift+0", () => {
+    if (!appShow) {
+      appShow = true;
+      mainWindow.show();
+    } else {
+      appShow = false;
+      mainWindow.hide();
+    }
+  });
+
+  /*
+  globalShortcut.register(copyShortcut, () => {
+    let as = clipboard.read();
+    console.log(as);
+  });
+   */
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(() => {
+  createWindow();
+});
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", function() {
+  if (process.platform !== "darwin") app.quit();
+});
 
-app.on('activate', function () {
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
-})
+app.on("activate", function() {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
